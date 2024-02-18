7930c4cbace24db22dc8e7b296b14b77e34b7697
AndreaTerenz <andrea.terenziani.at@gmail.com>
2024-02-19 00:25:04 +0100

    Pipenv test

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import time
 
 import flask
+import shortuuid
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,7 +9,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return flask.render_template("index.html", funny=69, time=time.time())
+    return flask.render_template("index.html",
+                                 funny=69,
+                                 time=time.time(),
+                                 suuid=shortuuid.uuid())
 
 
 if __name__ == '__main__':
